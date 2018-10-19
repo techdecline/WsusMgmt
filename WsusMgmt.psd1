3a7a748bb40bbdc3e3ff751de5ed36e400be80b6
Schuchardt <schuchardt@softed.de>
2018-10-19 12:51:31 +0200

    First code push

---
+++ WsusMgmt.psd1
@@ -0,0 +1,122 @@
+#
+# Modulmanifest fÃ¼r das Modul "WsusMgmt"
+#
+# Generiert von: schuchardt
+#
+# Generiert am: 26.04.2018
+#
+
+@{
+
+    # Die diesem Manifest zugeordnete Skript- oder BinÃ¤rmoduldatei.
+    RootModule = 'WsusMgmt.psm1'
+
+    # Die Versionsnummer dieses Moduls
+    ModuleVersion = '0.2.0'
+
+    # UnterstÃ¼tzte PSEditions
+    # CompatiblePSEditions = @()
+
+    # ID zur eindeutigen Kennzeichnung dieses Moduls
+    GUID = 'a35db157-4e06-4bb4-b8ec-618eb61cac93'
+
+    # Autor dieses Moduls
+    Author = 'Cornelius Schuchardt'
+
+    # Unternehmen oder Hersteller dieses Moduls
+    CompanyName = 'SoftEd Systems GmbH'
+
+    # UrheberrechtserklÃ¤rung fÃ¼r dieses Modul
+    Copyright = '(c) 2018 Cornelius Schuchardt. All rights reserved.'
+
+    # Beschreibung der von diesem Modul bereitgestellten Funktionen
+    # Description = ''
+
+    # Die fÃ¼r dieses Modul mindestens erforderliche Version des Windows PowerShell-Moduls
+    # PowerShellVersion = ''
+
+    # Der Name des fÃ¼r dieses Modul erforderlichen Windows PowerShell-Hosts
+    # PowerShellHostName = ''
+
+    # Die fÃ¼r dieses Modul mindestens erforderliche Version des Windows PowerShell-Hosts
+    # PowerShellHostVersion = ''
+
+    # Die fÃ¼r dieses Modul mindestens erforderliche Microsoft .NET Framework-Version. Diese erforderliche Komponente ist nur fÃ¼r die PowerShell Desktop-Edition gÃ¼ltig.
+    # DotNetFrameworkVersion = ''
+
+    # Die fÃ¼r dieses Modul mindestens erforderliche Version der CLR (Common Language Runtime). Diese erforderliche Komponente ist nur fÃ¼r die PowerShell Desktop-Edition gÃ¼ltig.
+    # CLRVersion = ''
+
+    # Die fÃ¼r dieses Modul erforderliche Prozessorarchitektur ("Keine", "X86", "Amd64").
+    # ProcessorArchitecture = ''
+
+    # Die Module, die vor dem Importieren dieses Moduls in die globale Umgebung geladen werden mÃ¼ssen
+    RequiredModules = "UpdateServices"
+
+    # Die Assemblys, die vor dem Importieren dieses Moduls geladen werden mÃ¼ssen
+    # RequiredAssemblies = @()
+
+    # Die Skriptdateien (PS1-Dateien), die vor dem Importieren dieses Moduls in der Umgebung des Aufrufers ausgefÃ¼hrt werden.
+    # ScriptsToProcess = @()
+
+    # Die Typdateien (.ps1xml), die beim Importieren dieses Moduls geladen werden sollen
+    # TypesToProcess = @()
+
+    # Die Formatdateien (.ps1xml), die beim Importieren dieses Moduls geladen werden sollen
+    # FormatsToProcess = @()
+
+    # Die Module, die als geschachtelte Module des in "RootModule/ModuleToProcess" angegebenen Moduls importiert werden sollen.
+    # NestedModules = @()
+
+    # Aus diesem Modul zu exportierende Funktionen. Um optimale Leistung zu erzielen, verwenden Sie keine Platzhalter und lÃ¶schen den Eintrag nicht. Verwenden Sie ein leeres Array, wenn keine zu exportierenden Funktionen vorhanden sind.
+    FunctionsToExport = '*'
+
+    # Aus diesem Modul zu exportierende Cmdlets. Um optimale Leistung zu erzielen, verwenden Sie keine Platzhalter und lÃ¶schen den Eintrag nicht. Verwenden Sie ein leeres Array, wenn keine zu exportierenden Cmdlets vorhanden sind.
+    CmdletsToExport = '*'
+
+    # Die aus diesem Modul zu exportierenden Variablen
+    VariablesToExport = '*'
+
+    # Aus diesem Modul zu exportierende Aliase. Um optimale Leistung zu erzielen, verwenden Sie keine Platzhalter und lÃ¶schen den Eintrag nicht. Verwenden Sie ein leeres Array, wenn keine zu exportierenden Aliase vorhanden sind.
+    AliasesToExport = '*'
+
+    # Aus diesem Modul zu exportierende DSC-Ressourcen
+    # DscResourcesToExport = @()
+
+    # Liste aller Module in diesem Modulpaket
+    # ModuleList = @()
+
+    # Liste aller Dateien in diesem Modulpaket
+    # FileList = @()
+
+    # Die privaten Daten, die an das in "RootModule/ModuleToProcess" angegebene Modul Ã¼bergeben werden sollen. Diese kÃ¶nnen auch eine PSData-Hashtabelle mit zusÃ¤tzlichen von PowerShell verwendeten Modulmetadaten enthalten.
+    PrivateData = @{
+
+        PSData = @{
+
+            # 'Tags' wurde auf das Modul angewendet und unterstÃ¼tzt die Modulermittlung in Onlinekatalogen.
+            # Tags = @()
+
+            # Eine URL zur Lizenz fÃ¼r dieses Modul.
+            # LicenseUri = ''
+
+            # Eine URL zur Hauptwebsite fÃ¼r dieses Projekt.
+            # ProjectUri = ''
+
+            # Eine URL zu einem Symbol, das das Modul darstellt.
+            # IconUri = ''
+
+            # 'ReleaseNotes' des Moduls
+            # ReleaseNotes = ''
+
+        } # Ende der PSData-Hashtabelle
+
+    } # Ende der PrivateData-Hashtabelle
+
+    # HelpInfo-URI dieses Moduls
+    # HelpInfoURI = ''
+
+    # StandardprÃ¤fix fÃ¼r Befehle, die aus diesem Modul exportiert werden. Das StandardprÃ¤fix kann mit "Import-Module -Prefix" Ã¼berschrieben werden.
+    # DefaultCommandPrefix = ''
+
+    }
---
+++ WsusMgmt.psm1
@@ -0,0 +1,153 @@
+# Implement your module commands in this script.
+function Start-WsusIniialization {
+    [CmdletBinding()]
+    param (
+        # Proxy Server and Port
+        [Parameter(Mandatory=$false)]
+        [ValidatePattern("^.*:.*$")]
+        [String]
+        $ProxyAddress,
+
+        # Configure desired Targeting Mode
+        [Parameter(Mandatory=$false)]
+        [ValidateSet("Client","Server")]
+        [String]
+        $TargetingMode
+    )
+
+    process {
+        $wsus = get-wsusserver
+        Set-WsusServerSynchronization -SyncFromMU
+        $wsusConfig = $wsus.GetConfiguration()
+        $wsusConfig.AllUpdateLanguagesEnabled = $false
+        $wsusConfig.SetEnabledUpdateLanguages("en")
+        if ($ProxyAddress) {
+            $proxyServerPort = ($ProxyAddress -split ":")[1]
+            $proxyServerName = ($ProxyAddress -split ":")[0]
+            $wsusConfig.UseProxy = $true
+            $wsusConfig.ProxyName = $proxyServerName
+            $wsusConfig.ProxyServerPort = $proxyServerPort
+        }
+
+        $wsusConfig.Save()
+
+        # Start Synchronisierung und warten auf Ergebnis
+        $subscription = $wsus.GetSubscription()
+        $subscription.StartSynchronizationForCategoryOnly()
+        $subscription.GetSynchronizationStatus()
+        while ($subscription.GetSynchronizationStatus() -eq "Running")  {
+            Write-Host "." -NoNewline
+            Start-Sleep -Seconds 5
+        }
+    }
+}
+
+function Sync-WsusApprovedUpdates {
+    param (
+        [String]$OldWsusServerName
+    )
+
+    $newWsus = Get-WsusServer
+    $oldWsus = Get-WsusServer -Name $OldWsusServerName -PortNumber 8530
+    $updateArr = Get-WsusUpdate -UpdateServer $newWsus
+
+    foreach ($update in $updateArr) {
+        $oldUpdate = Get-WsusUpdate -UpdateServer $oldWsus -UpdateId $update.UpdateId
+        if ($oldUpdate.Approved -eq "Install") {
+            if (Get-WsusUpdateEulaApprovalRequirement -WsusUpdate $update) {
+                Approve-WsusUpdateLicense -WsusUpdate $update
+            }
+            Approve-WsusUpdate -Update $update -Action Install -TargetGroupName "Alle Computer"
+        }
+    }
+}
+
+function Sync-WsusCategories {
+    param (
+        [String]$OldWsusServerName
+    )
+
+    $oldWsusServer = Get-WsusServer -Name $OldWsusServerName -PortNumber 8530
+    $oldSubscription = $oldWsusServer.GetSubscription()
+    $oldProducts = $oldSubscription.GetUpdateCategories()
+    $NewWsusServer = Get-WsusServer
+    $validProducts = $oldProducts | Where-Object {(Get-WsusProduct).Product.Title -contains $_.Title}
+    $updateCollection = New-Object -TypeName Microsoft.UpdateServices.Administration.UpdateCategoryCollection
+    $updateCollection.AddRange($validProducts)
+    $newSubscription = $newWsusServer.GetSubscription()
+    $newSubscription.SetUpdateCategories($updateCollection)
+    $newSubscription.Save()
+}
+
+function Sync-WsusClassifications {
+    param (
+        [String]$OldWsusServerName
+    )
+
+    $oldWsusServer = Get-WsusServer -Name $OldWsusServerName -PortNumber 8530
+    $oldSubscription = $oldWsusServer.GetSubscription()
+    $oldProducts = $oldSubscription.GetUpdateClassifications()
+    $NewWsusServer = Get-WsusServer
+    #$validProducts = $oldProducts | Where-Object {(Get-WsusProduct).Product.Title -contains $_.Title}
+    #$updateCollection = New-Object -TypeName Microsoft.UpdateServices.Administration.UpdateClassificationCollection
+    #$updateCollection.AddRange($validProducts)
+    $newSubscription = $newWsusServer.GetSubscription()
+    $newSubscription.SetUpdateClassifications($oldProducts)
+    $newSubscription.Save()
+}
+
+function Sync-WsusDeclinedUpdates {
+    param (
+        [String]$OldWsusServerName
+    )
+
+    $oldWsusServer = Get-WsusServer -Name $OldWsusServerName -PortNumber 8530
+
+    Get-WsusUpdate -UpdateServer $OldWsusServer -Approval Declined | ForEach-Object {
+        try {
+            $update = Get-WsusUpdate -UpdateId $_.UpdateId -ErrorAction SilentlyContinue
+            $update.update.Decline()
+            Write-Host "Declined update: $($update.UpdateId)"
+        }
+        catch [Microsoft.UpdateServices.Administration.WsusObjectNotFoundException] {
+            # placeholder
+        }
+    }
+}
+
+function Sync-WsusTargetGroup {
+    param (
+        [String]$OldWsusServerName
+    )
+
+    $oldWsusServer = Get-WsusServer -Name $OldWsusServerName -PortNumber 8530
+    $wsusServer = Get-WsusServer
+    $oldTargetGroupArr = $oldWsusServer.GetComputerTargetGroups() | Where-Object {$_.Name -ne "Nicht zugewiesene Computer"}
+
+    foreach ($targetGroup in $oldTargetGroupArr) {
+        $wsusServer.CreateComputerTargetGroup($targetGroup.Name)
+        $targetGroup.Name
+    }
+}
+
+function Get-WsusUpdateEulaApprovalRequirement {
+    param (
+        [Microsoft.UpdateServices.Commands.WsusUpdate]$WsusUpdate
+    )
+
+    $needsApproval = $WsusUpdate.Update.RequiresLicenseAgreementAcceptance
+    return $needsApproval
+}
+
+function Approve-WsusUpdateLicense {
+    param (
+        [Microsoft.UpdateServices.Commands.WsusUpdate]$WsusUpdate
+    )
+
+    $approvalResult = $WsusUpdate.Update.AcceptLicenseAgreement()
+}
+
+# Export only the functions using PowerShell standard verb-noun naming.
+# Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
+# This improves performance of command discovery in PowerShell.
+Export-ModuleMember -Function Start-WsusIniialization,Sync-WsusApprovedUpdates,Sync-WsusCategories,Sync-WsusDeclinedUpdates,Sync-WsusTargetGroup
