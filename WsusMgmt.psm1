f5ad1de2086ef6342049edf29bca4831a555bd8f
Cornelius Schuchardt <cornelius.schuchardt@gmail.com>
2018-10-25 11:58:23 +0200

    Update WsusMgmt.ps1: Typo fixed

--- WsusMgmt.psm1
+++ WsusMgmt.psm1
@@ -168,4 +168,4 @@ function Approve-WsusUpdateLicense {
 # Export only the functions using PowerShell standard verb-noun naming.
 # Be sure to list each exported functions in the FunctionsToExport field of the module manifest file.
 # This improves performance of command discovery in PowerShell.
-Export-ModuleMember -Function Start-WsusIniialization,Sync-WsusApprovedUpdates,Sync-WsusCategories,Sync-WsusDeclinedUpdates,Sync-WsusTargetGroup
+Export-ModuleMember -Function Start-WsusInitialization,Sync-WsusApprovedUpdates,Sync-WsusCategories,Sync-WsusDeclinedUpdates,Sync-WsusTargetGroup
