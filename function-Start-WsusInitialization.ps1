a2e94dd34f7c41766cd6917d237a1be7666a34d3
techdecline <cornelius.schuchardt@gmail.com>
2018-10-26 02:09:48 -0700

    AsJob

--- function-Start-WsusInitialization.ps1
+++ function-Start-WsusInitialization.ps1
@@ -1,32 +1,37 @@
-    <#
-    .Synopsis
-    Start initial WSUS Sync with Microsoft Update after WSUS Installation.
-    .DESCRIPTION
-    Start initial WSUS Sync with Microsoft Update after WSUS Installation. Optionally, a proxy server can be added.
-    .EXAMPLE
-    PS> Start-WsusInitialization
+<#
+.Synopsis
+Start initial WSUS Sync with Microsoft Update after WSUS Installation.
+.DESCRIPTION
+Start initial WSUS Sync with Microsoft Update after WSUS Installation. Optionally, a proxy server can be added.
+.EXAMPLE
+PS> Start-WsusInitialization
 
-    This command will start the initial MU sync without using a proxy server.
-    .EXAMPLE
-    PS> Start-WsusInitialization -ProxyAddress proxy:8080
+This command will start the initial MU sync without using a proxy server.
+.EXAMPLE
+PS> Start-WsusInitialization -ProxyAddress proxy:8080
 
-    Using this command the initial MU sync will be started using a proxy server called "proxy" and TCP Port 8080.
-    .INPUTS
-    .NOTES
-    Author: Cornelius Schuchardt
-    Link: https://github.com/techdecline/WsusMgmt
-    #>
-    function Start-WsusInitialization {
-        [CmdletBinding(DefaultParameterSetName="Default")]
-        param (
-            # Proxy Server and Port
-            [Parameter(Mandatory,ParameterSetName="WithProxy")]
-            [ValidatePattern("^.*:.*$")]
-            [String]
-            $ProxyAddress
-        )
+Using this command the initial MU sync will be started using a proxy server called "proxy" and TCP Port 8080.
+.INPUTS
+.NOTES
+Author: Cornelius Schuchardt
+Link: https://github.com/techdecline/WsusMgmt
+#>
+function Start-WsusInitialization {
+    [CmdletBinding(DefaultParameterSetName="Default")]
+    param (
+        # Proxy Server and Port
+        [Parameter(Mandatory,ParameterSetName="WithProxy",Position=0)]
+        [ValidatePattern("^.*:.*$")]
+        [String]
+        $ProxyAddress,
 
-        process {
+        [Parameter(Mandatory=$false)]
+        [switch]$AsJob
+    )
+
+    process {
+        [scriptblock]$proxyFunction = {
+            param ($ProxyAddress)
             $wsus = get-wsusserver
             Set-WsusServerSynchronization -SyncFromMU
             $wsusConfig = $wsus.GetConfiguration()
@@ -51,4 +56,12 @@
                 Start-Sleep -Seconds 5
             }
         }
+
+        if ($AsJob) {
+            Start-Job -ScriptBlock $proxyFunction -ArgumentList $ProxyAddress
+        }
+        else {
+            . $proxyFunction $ProxyAddress
+        }
     }
+}
